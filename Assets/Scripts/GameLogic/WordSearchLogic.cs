7ec50c240fad33b22765af29841c55858f19a3f9
VincentHayen <vincent.hayen@student.howest.be>
2023-01-12 09:26:08 +0100

    Added word search list of words

--- Assets/Scripts/GameLogic/WordSearchLogic.cs
+++ Assets/Scripts/GameLogic/WordSearchLogic.cs
@@ -18,6 +18,22 @@ public class WordSearchLogic : MonoBehaviour, IPuzzleLogic
     private List<GameObject> _inputFields = new List<GameObject>();
     
     private List<int> _possibleInputs = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 };
+
+    //https://www.thefreedictionary.com/3-letter-words.htm
+    private List<string> _3characterWords = new List<string>() { "cat", "dog", "boy", "dad", "eye", "elk", "git", "mug", "mad", "ham",
+                                                                 "hub", "far", "box", "big", "jam", "new", "rem", "spa", "yes", "ufo",
+                                                                 "top", "tin", "sir", "owl", "zoo", "tea", "sea", "paw", "nun", "bug"};
+
+    //https://www.thefreedictionary.com/4-letter-words.htm
+    private List<string> _4CharacterWords = new List<string>() { "anti", "half", "isle", "leaf", "town", "list", "pine", "scar", "demo", "gift",
+                                                                 "gone", "lane", "idle", "halt", "kirk", "grip", "euro", "cube", "rank", "tier",
+                                                                 "year", "yard", "yarn", "trim", "slam", "next", "love", "meal", "boil", "ball"};
+
+    //https://www.thefreedictionary.com/5-letter-words.htm
+    private List<string> _5CharacterWords = new List<string>() { "beach", "couch", "first", "great", "later", "index", "guess", "panic", "level", "prime",
+                                                                 "scene", "solid", "total", "small", "pitch", "match", "mercy", "yield", "grand", "logic",
+                                                                  "honey", "ghost", "fence", "drill", "clear", "cabin", "about", "reply", "skill", "state"};
+
     public List<int> PossibleInputs { get { return _possibleInputs; } set { value = _possibleInputs; } }
 
     private List<string> _shortWordsInput = new List<string>() { };
